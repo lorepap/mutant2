9ce660f5de3ec43786d3dc0f193ab0d7ae23f100
lorepap <p.lore96@gmail.com>
2024-02-19 20:19:51 -0600

    tensorflow support - fix required

--- src/mab/runner.py
+++ src/mab/runner.py
@@ -6,28 +6,41 @@ from typing import Any
 import numpy as np
 from utilities import context, utils
 from utilities.callbacks import TrainingCallback, ModelCheckpoint
-from keras.optimizers import Adam
-from mab.mabagent import MabAgent
-from mab.environment import MabEnvironment
+
+import tensorflow as tf
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.specs import tensor_spec
+from tf_agents.specs import array_spec
+from tf_agents.trajectories import trajectory
+from tf_agents.trajectories import time_step as ts
+
+from mab.mab_tf_agent import MabAgent
+from mab.mab_tf_environment import MabEnvironment
+
 from comm.comm import CommManager
 
 from tensorflow.python.keras.optimizer_v2 import optimizer_v2
 from mab.moderator import Moderator
 import yaml
-import logging
 
 
 class MabRunner():
     def __init__(self, policies, min_rtt, bw, bdp_mult, checkpoint_filepath: str = None, log=True):
 
         # List of CCA IDs to map the policies to the action 
-        self.policies = policies
+        self.policies_id = policies
         
         # running params
         with open('config/train.yml', 'r') as file:
             config = yaml.safe_load(file)
         self.config = config
         self.timestamp = utils.time_to_str()
+        self.feature_settings = utils.parse_features_config()
+        self.feature_names = self.feature_settings['kernel_info'] # List of features
+        self.stat_features= self.feature_settings['train_stat_features']
+        self.non_stat_features = self.feature_settings['train_non_stat_features']
+        self.obs_size = len(self.non_stat_features) + len(self.stat_features)*3*3
 
         # dir
         self.log_dir = "log/mab"
@@ -40,9 +53,9 @@ class MabRunner():
 
         if not policies:
             self.proto_config = utils.parse_protocols_config()
-            self.policies = list(self.proto_config.keys())
+            self.policies_id = list(self.proto_config.keys())
 
-        self.nchoices = len(self.policies)
+        self.nchoices = len(self.policies_id)
         if 'lr' in config:
             self.lr = config['lr']
         self.moderator = Moderator()
@@ -54,26 +67,29 @@ class MabRunner():
 
         self.training_time = None
         self.step_wait_time = config['step_wait_seconds']
-        self.steps_per_episode = config['steps_per_episode']
-        self.num_fields_kernel = config['num_fields_kernel']
-        self.training_steps = config['train_episodes'] * self.steps_per_episode
-
-        self.agent = MabAgent(self.nchoices, self.moderator)
+        self.num_steps = config['num_steps']
         
-        # TODO: checkpoint filepath to be changed for a better naming convention
-        self.agent.set_model_name(name=f'mab.{self.nchoices}actions.{self.training_steps}steps.{self.timestamp}')
-        if not checkpoint_filepath:
-            self.checkpoint_filepath = os.path.join(
-                context.entry_dir, self.log_dir, 'checkpoint', f'{self.model_path}.{self.agent.get_model_name()}')
+        # Define observation and action space
+        self.step_wait = config['step_wait_seconds']
+        max_samples = int(self.step_wait / 0.01) # 0.1 is the sampling frequency of the kernel
+        observation_spec = tensor_spec.BoundedTensorSpec(
+            shape=(self.obs_size,), dtype=tf.float32, minimum=0, maximum=np.inf)
+        time_step_spec = ts.time_step_spec(observation_spec)
+        action_spec = tensor_spec.BoundedTensorSpec(
+            dtype=tf.int32, shape=(), minimum=0, maximum=self.nchoices)
 
-        self.environment = MabEnvironment(self.cm, self.policies, config)
+        
+        # TF Agent
+        self.agent = MabAgent(time_step_spec=time_step_spec, action_spec=action_spec)
+        
+        # TF Environment
+        self.environment = MabEnvironment(observation_spec, action_spec, self.policies_id)
         self.environment.allow_save = log
-        self.training_steps = config['train_episodes'] * self.steps_per_episode
         self.now = time.time()
 
         # Settings
-        if self.policies:
-            map_proto = {p: action for action, p in zip(self.environment.map_proto.keys(), self.policies)}
+        if self.policies_id:
+            map_proto = {p: action for action, p in zip(self.environment.map_proto.keys(), self.policies_id)}
         else:
             map_proto = self.environment.map_proto
         self.settings = {'timestamp': self.timestamp, **config, 'action_mapping': map_proto, **self.environment.feature_settings}
@@ -85,7 +101,7 @@ class MabRunner():
         # Now a single flow between client and server is running
         # We can now set up the runner and start training the RL model    
         self.cm.init_kernel_communication()
-        self.cm.start_communication(client_tag=f'mab.{self.nchoices}actions.{self.training_steps}steps')
+        self.cm.start_communication(client_tag=f'mab.{self.nchoices}actions.{self.num_steps}steps')
 
     def stop_communication(self):
         self.cm.stop_iperf_communication()
@@ -96,37 +112,37 @@ class MabRunner():
         os.makedirs(self.log_dir, exist_ok=True)
         os.makedirs(self.history_dir, exist_ok=True)
         os.makedirs(self.model_dir, exist_ok=True)
-
+    
     def train(self) -> Any:
+        steps_per_loop = 1 # @param
+        batch_size = 20
 
-        checkpoint_callback = ModelCheckpoint(
-            filepath=self.checkpoint_filepath
-        )
-
-        cb: TrainingCallback = TrainingCallback(
-            log_file_path=os.path.join(
-                context.entry_dir, 
-                self.history_dir,
-                f'{self.agent.get_model_name()}.json'
-            )
-        )
-
-        start = time.time()
-
-        self.agent.compile()
-        print(f"Training model {self.agent.get_model_name()} with {self.nchoices} arms...")
-        self.train_res = self.agent.fit(self.environment, nb_steps=self.training_steps, 
-                    callbacks=[cb],
-            visualize=False, verbose=2)
-
-        self.training_time = time.time() - start
         
-        self.history = self.train_res.history
+        replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            data_spec=self.agent.policy.trajectory_spec,
+            batch_size=batch_size, #TODO adjust batch_size (what it refers to?)
+            max_length=steps_per_loop)
 
-        # log the completed training
-        utils.update_log(os.path.join(self.log_dir, 'settings.json'), self.settings, 'success', self.training_time)
+        observers = [replay_buffer.add_batch]
 
-        return self.history
+        driver = dynamic_step_driver.DynamicStepDriver(
+            env=self.environment,
+            policy=self.agent.collect_policy,
+            num_steps=steps_per_loop * batch_size,
+            observers=observers)
+
+        step = self.environment.reset()
+        print("observation:", step.observation)
+
+        for _ in range(self.num_steps):
+            action_step = self.agent.collect_policy.action(step)
+            next_step = self.environment.step(action_step.action)
+            experience = self.trajectory_for_bandit(step, action_step, next_step)
+            print(experience)
+            self.agent.train(replay_buffer.gather_all())
+            step = next_step
+        replay_buffer.clear()
+
 
     def test(self, episodes: int) -> None:
 
@@ -139,7 +155,7 @@ class MabRunner():
             )
         )
         
-        self.environment.enable_log_traces()
+        # self.environment.enable_log_traces()
         
         self.agent.test(self.environment,
                         nb_episodes=episodes, 
@@ -148,6 +164,20 @@ class MabRunner():
 
         # save logs
         # log_name, log_path = self.environment.save_log(model_id, 'log/mab/trace') #TODO: logging
+
+    # We need to add another dimension here because the agent expects the
+    # trajectory of shape [batch_size, time, ...], but in this tutorial we assume
+    # that both batch size and time are 1. Hence all the expand_dims.
+
+    def trajectory_for_bandit(self, initial_step, action_step, final_step):
+        return trajectory.Trajectory(observation=tf.expand_dims(initial_step.observation, 0),
+                                action=tf.expand_dims(action_step.action, 0),
+                                policy_info=action_step.info,
+                                reward=tf.expand_dims(final_step.reward, 0),
+                                discount=tf.expand_dims(final_step.discount, 0),
+                                step_type=tf.expand_dims(initial_step.step_type, 0),
+                                next_step_type=tf.expand_dims(final_step.step_type, 0))
+
 
     def save_history(self) -> None:
         path = os.path.join(
--- src/mab/tf_environment.py
+++ src/mab/tf_environment.py
@@ -1,18 +1,10 @@
 import abc
 import numpy as np
 import tensorflow as tf
-
-from tf_agents.agents import tf_agent
-from tf_agents.drivers import driver
+import traceback
 from tf_agents.environments import py_environment
-from tf_agents.environments import tf_environment
-from tf_agents.environments import tf_py_environment
-from tf_agents.policies import tf_policy
-from tf_agents.specs import array_spec
-from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts
-from tf_agents.trajectories import trajectory
-from tf_agents.trajectories import policy_step
+from tf_agents.specs import array_spec, tensor_spec 
 
 nest = tf.nest
 
@@ -20,7 +12,7 @@ class BanditPyEnvironment(py_environment.PyEnvironment):
 
   def __init__(self, observation_spec, action_spec):
     self._observation_spec = observation_spec
-    self._action_spec = action_spec
+    self._action_spec = action_spec 
     super(BanditPyEnvironment, self).__init__()
 
   # Helper functions.
@@ -29,6 +21,9 @@ class BanditPyEnvironment(py_environment.PyEnvironment):
 
   def observation_spec(self):
     return self._observation_spec
+  
+  def reward_spec(self):
+     return tensor_spec.TensorSpec(shape=(), dtype=np.float32, name='reward')
 
   def _empty_observation(self):
     return tf.nest.map_structure(lambda x: np.zeros(x.shape, x.dtype),
@@ -37,6 +32,7 @@ class BanditPyEnvironment(py_environment.PyEnvironment):
   # These two functions below should not be overridden by subclasses.
   def _reset(self):
     """Returns a time step containing an observation."""
+    self._init_communication()
     return ts.restart(self._observe(), batch_size=self.batch_size)
 
   def _step(self, action):
@@ -53,3 +49,36 @@ class BanditPyEnvironment(py_environment.PyEnvironment):
   def _apply_action(self, action):
     """Applies `action` to the Environment and returns the corresponding reward.
     """
+
+  def _init_communication(self):
+        if not self.initiated:
+            print("Initiating communication...")
+            self.kernel_thread.start()
+            print("Communication initiated")
+            self.initiated = True
+
+  def _end_communication(self):
+      try:
+          print("Closing communication...")
+
+          # Close thread
+          self.kernel_thread.exit()
+          self.initiated = False
+
+          print("Communication closed")
+
+      except Exception as err:
+          print(traceback.format_exc())
+
+  def _recv_data(self):
+      msg = self.netlink_communicator.receive_msg()
+      data = self.netlink_communicator.read_netlink_msg(msg)
+      split_data = data.decode(
+          'utf-8').split(';')[:self.num_fields_kernel]
+      return list(map(int, split_data))
+
+  def _read_data(self):
+      kernel_info = self.kernel_thread.queue.get()
+      self.kernel_thread.queue.task_done()
+      return kernel_info
+
