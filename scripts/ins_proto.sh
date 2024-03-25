a3f2485230ebba1ccfac18442a9d0993ee0497bf
lorepap <p.lore96@gmail.com>
2024-03-25 17:14:03 -0500

    kernel setup

--- scripts/ins_proto.sh
+++ scripts/ins_proto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/linux-5.4.231-mutant/net/ipv4
+echo '--- Inserting protocols as modules ---'
 sudo insmod tcp_hybla.ko
 sudo insmod tcp_bbr.ko
 sudo insmod tcp_westwood.ko
@@ -12,4 +13,3 @@ sudo insmod tcp_bic.ko
 sudo insmod tcp_htcp.ko
 sudo insmod tcp_highspeed.ko
 sudo insmod tcp_illinois.ko
-
